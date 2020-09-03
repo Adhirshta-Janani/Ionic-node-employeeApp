e43a175bddccf3a02b53788defdd37a79ed859d8
Adhirshta <janani@animapp.in>
2020-09-03 17:57:19 +0530

    Initial Push

---
+++ mobileApp/empApp/src/pages/home/home.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
+import { EmployeeCreatePage } from './../employee-create/employee-create';
+import {  EmployeeServiceProvider } from './../../providers/employee-service/employee-service';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  employees: any[];
+ 
+  constructor(public navCtrl: NavController, public EmployeeProvider:EmployeeServiceProvider ) {
+
+  }
+
+  ionViewDidLoad(){
+
+   this.EmployeeProvider.getEmployeeDetails().subscribe(data => {
+    if (data.success) {
+      console.log("success");
+      this.employees = [data.employeeDetails];
+      console.log(this.employees);
+    }
+  })
+
+  }
+
+  add(){
+    this.navCtrl.push(  EmployeeCreatePage );
+  }
+
+  delete(id:any){
+
+    var employee_id = id;
+    this.EmployeeProvider.deleteEmployeeDetails(employee_id).subscribe(data=>{
+      if(data.success){
+
+        this.ionViewDidLoad();
+
+      }
+    })
+
+  }
+   
+}
